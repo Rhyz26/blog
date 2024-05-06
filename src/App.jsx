07b83336fcbde5e43077fe2eb650824bc14ba3ac
Raymond <njagala.raymond@gmail.com>
2024-05-06 17:29:13 +0300

    added some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,29 @@
-
-import Header from './components/Header'
-import Footer from './components/Footer'
-import { BrowserRouter, Routes, Route } from "react-router-dom"; 
-import About from './components/About';
-import Boxing from './components/Boxing';
-import Home from './components/Home';
-import Scores from './components/Scores';
-import World from './components/World'
-import Wwe from './components/Wwe';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Boxing from "./components/Boxing";
+import Home from "./components/Home";
+import Scores from "./components/Scores";
+import World from "./components/World";
+import Wwe from "./components/Wwe";
 
 function App() {
-  
-
   return (
-  
     <BrowserRouter>
-    <Header/>
-    
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/about" element={<About/>} />
-      <Route path="/worldfootball" element={<World/>} />
-      
-      <Route path="/wwe" element={<Wwe/>} />
-      <Route path="/boxing" element={<Boxing/>} />
-      <Route path="/scores" element={<Scores/>} />
-      
-    </Routes>
-    <Footer/>
+      <Header />
 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/worldfootball" element={<World />} />
+
+        <Route path="/wwe" element={<Wwe />} />
+        <Route path="/boxing" element={<Boxing />} />
+        <Route path="/scores" element={<Scores />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
-    
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,58 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "@mantine/core/styles.css";
+import { Button } from "@mantine/core";
 
+import { MantineProvider } from "@mantine/core";
 
 function Header() {
   return (
-    <div>
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
+    
+      <div className="header">
+        <ul>
+          <li>
+            <Link to="/">N R K</Link>
+          </li>
 
-            <li>
-              <Link to="/about">About</Link>
-            </li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
 
-            <li>
-              <Link to="/worldfootball">WORLD FOOTBALL</Link>
-            </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
 
-            <li>
-              <Link to="/wwe">WWE</Link>
-            </li>
+          <li>
+            <Link to="/worldfootball">WORLD FOOTBALL</Link>
+          </li>
 
-            <li>
-              <Link to="/boxing">BOXING</Link>
-            </li>
+          <li>
+            <Link to="/wwe">WWE</Link>
+          </li>
 
-            <li>
-              <Link to="/scores">SCORES</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
+          <li>
+            <Link to="/boxing">BOXING</Link>
+          </li>
+
+          <li>
+            <Link to="/scores">SCORES</Link>
+          </li>
+        </ul>
+     
+
+      <div className="button">
+        <MantineProvider>
+          <Button
+            variant="filled"
+            color="rgba(8, 255, 61, 1)"
+            size="sm"
+            radius="xl"
+            
+          >
+            Account
+          </Button>
+        </MantineProvider>
+      </div>
     </div>
   );
 }
